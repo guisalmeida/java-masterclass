7b59f163fef807682eb598200093dacf19d8da42
guisalmeida <guisalmeida.dev@gmail.com>
2026-02-12 22:57:43 +0000

    [ByteShortIntLong] feat: add long

--- ByteShortIntLong/src/academy/learnprogramming/Main.java
+++ ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println("Busted minimum value = " + (myMinValue - 1));
 
         // We can add underscore to differentiate the decimal values:
-        // int myMaxIntTest = 2_147_483_647;
+        int myMaxIntTest = 2_147_483_647;
 
         byte myMinByteValue = Byte.MIN_VALUE;
         byte myMaxByteValue = Byte.MAX_VALUE;
@@ -23,5 +23,13 @@ public class Main {
         short myMaxShortValue = Short.MAX_VALUE;
         System.out.println("Short minimum value: " + myMinShortValue);
         System.out.println("Short maximum value: " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long bigLongValue = myMaxIntTest * myLongValue;
+        System.out.println("Long bigLongValue value: " + bigLongValue);
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long minimum value: " + myMinLongValue);
+        System.out.println("Long maximum value: " + myMaxLongValue);
     }
 }
