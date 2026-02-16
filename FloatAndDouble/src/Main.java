ac1f5ca0b8885697cd2a3ebb582e9f530b5b6630
guisalmeida <guisalmeida.dev@gmail.com>
2026-02-16 13:32:56 +0000

    [FloatAndDouble] feat: add variables

---
+++ FloatAndDouble/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+        float myMinFloat = Float.MIN_VALUE;
+        float myMaxFloat  = Float.MAX_VALUE;
+
+        System.out.println("myMinFloat:" + myMinFloat);
+        System.out.println("myMaxFloat:" + myMaxFloat);
+
+        double myMinDouble = Double.MIN_VALUE;
+        double myMaxDouble  = Double.MAX_VALUE;
+
+        System.out.println("myMinDouble:" + myMinDouble);
+        System.out.println("myMaxDouble:" + myMaxDouble);
+
+        int myIntValue = 5;
+        float myFloatValue = (float) 5.25f;
+        double myDoubleValue = 5.25d;
+    }
+}
