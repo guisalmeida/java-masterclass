29d7520b1168da22c2a68270a580dad6a7d082ce
guisalmeida <guisalmeida.dev@gmail.com>
2026-02-16 13:43:38 +0000

    [FloatAndDouble] feat: add division

--- FloatAndDouble/src/Main.java
+++ FloatAndDouble/src/Main.java
@@ -2,19 +2,22 @@
 public class Main {
     public static void main(String[] args) {
         float myMinFloat = Float.MIN_VALUE;
-        float myMaxFloat  = Float.MAX_VALUE;
+        float myMaxFloat = Float.MAX_VALUE;
 
         System.out.println("myMinFloat:" + myMinFloat);
         System.out.println("myMaxFloat:" + myMaxFloat);
 
         double myMinDouble = Double.MIN_VALUE;
-        double myMaxDouble  = Double.MAX_VALUE;
+        double myMaxDouble = Double.MAX_VALUE;
 
         System.out.println("myMinDouble:" + myMinDouble);
         System.out.println("myMaxDouble:" + myMaxDouble);
 
-        int myIntValue = 5;
-        float myFloatValue = (float) 5.25f;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+        System.out.println("myIntValue:" + myIntValue);
+        System.out.println("myFloatValue:" + myFloatValue);
+        System.out.println("myDoubleValue:" + myDoubleValue);
     }
 }
