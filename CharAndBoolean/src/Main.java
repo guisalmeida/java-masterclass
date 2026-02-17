ed0183480d0e5f1061d9980f018c9f8a5539799f
guisalmeida <guisalmeida.dev@gmail.com>
2026-02-17 10:29:55 +0000

    [CharAndBoolean] feat: add variables

---
+++ CharAndBoolean/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myCopyrightChar = '\u00A9';
+
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myCopyrightChar);
+    }
+}
