7e157700112934ee3010f441ecbfed23b85dfd49
guisalmeida <guisalmeida.dev@gmail.com>
2026-03-11 11:39:15 +0000

    refactor: maven modules

---
+++ ByteShortIntLong/src/main/java/com/guisalmeida/Main.java
@@ -0,0 +1,40 @@
+package com.guisalmeida;
+
+public class Main {
+    public static void main(String[] args) {
+        int myMinValue = Integer.MIN_VALUE;
+        int myMaxValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer minimum value: " + myMinValue);
+        System.out.println("Integer maximum value: " + myMaxValue);
+
+        System.out.println("Busted maximum value = " + (myMaxValue + 1));
+        System.out.println("Busted minimum value = " + (myMinValue - 1));
+
+        // We can add underscore to differentiate the decimal values:
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte minimum value: " + myMinByteValue);
+        System.out.println("Byte maximum value: " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short minimum value: " + myMinShortValue);
+        System.out.println("Short maximum value: " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long bigLongValue = myMaxIntTest * myLongValue;
+        System.out.println("Long bigLongValue value: " + bigLongValue);
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long minimum value: " + myMinLongValue);
+        System.out.println("Long maximum value: " + myMaxLongValue);
+
+
+        int myTotal = (myMinValue / 2);
+        byte myTotalByte = (byte) (myMinByteValue / 2);
+        short myTotalShort = (short) (myMinShortValue / 2);
+    }
+}
---
+++ FloatAndDouble/src/main/java/com/guisalmeida/Main.java
@@ -0,0 +1,29 @@
+package com.guisalmeida;
+
+public class Main {
+    public static void main(String[] args) {
+        float myMinFloat = Float.MIN_VALUE;
+        float myMaxFloat = Float.MAX_VALUE;
+
+        System.out.println("myMinFloat:" + myMinFloat);
+        System.out.println("myMaxFloat:" + myMaxFloat);
+
+        double myMinDouble = Double.MIN_VALUE;
+        double myMaxDouble = Double.MAX_VALUE;
+
+        System.out.println("myMinDouble:" + myMinDouble);
+        System.out.println("myMaxDouble:" + myMaxDouble);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+        System.out.println("myIntValue:" + myIntValue);
+        System.out.println("myFloatValue:" + myFloatValue);
+        System.out.println("myDoubleValue:" + myDoubleValue);
+
+        // Challenge:
+        double pounds = 200d;
+        double convertedKg = pounds * 0.45359237d;
+        System.out.println("Conversion result:" + convertedKg);
+    }
+}
