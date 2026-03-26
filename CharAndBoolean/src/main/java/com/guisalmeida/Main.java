ba072dabb6cd8b7f0fdd76854b6b41b7b2fe3bed
guisalmeida <guisalmeida.dev@gmail.com>
2026-03-26 22:19:19 +0000

    chore: add Methods module and update project configurations

--- CharAndBoolean/src/main/java/com/guisalmeida/Main.java
+++ CharAndBoolean/src/main/java/com/guisalmeida/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         char myChar = 'D';
         char myUnicodeChar = '\u0044';
-        char myCopyrightChar = '\u00A9';
+        char myDecimalChar = 68;
 
         System.out.println(myChar);
         System.out.println(myUnicodeChar);
-        System.out.println(myCopyrightChar);
+        System.out.println(myDecimalChar);
     }
 }
